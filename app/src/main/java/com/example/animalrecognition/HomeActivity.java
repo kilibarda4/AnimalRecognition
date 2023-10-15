608750fc5bbec20534d9bbd28465a3f4a726f89b
kilibarda4 <nxk4192@mavs.uta.edu>
2023-10-15 00:13:34 -0500

    Home Activity xml

--- app/src/main/java/com/example/animalrecognition/HomeActivity.java
+++ app/src/main/java/com/example/animalrecognition/HomeActivity.java
@@ -3,9 +3,13 @@ package com.example.animalrecognition;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
 
+    Button startRecordingBtn, stopRecordingBtn;
+    TextView result;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/example/animalrecognition/LoginActivity.java
+++ app/src/main/java/com/example/animalrecognition/LoginActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.text.TextUtils;
+import android.text.method.HideReturnsTransformationMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
