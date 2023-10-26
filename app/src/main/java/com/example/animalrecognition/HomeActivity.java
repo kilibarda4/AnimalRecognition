6d695d225a1ba9fe4d9140c5d8eaa0e0547d7830
kilibarda4 <nxk4192@mavs.uta.edu>
2023-10-25 22:56:44 -0500

    Home Activity fragments

--- app/src/main/java/com/example/animalrecognition/HomeActivity.java
+++ app/src/main/java/com/example/animalrecognition/HomeActivity.java
@@ -1,18 +1,54 @@
 package com.example.animalrecognition;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.example.animalrecognition.databinding.ActivityHomeBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeActivity extends AppCompatActivity {
 
     Button startRecordingBtn, stopRecordingBtn;
     TextView result;
+    BottomNavigationView bottomNavigationView;
+    ActivityHomeBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
+            if(id == R.id.navigation_home) {
+                Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                startActivity(intent);
+            }
+            else if(id == R.id.navigation_profile) {
+                replaceFragment(new ProfileFragment());
+            }
+            else if(id == R.id.navigation_stats) {
+                replaceFragment(new StatsFragment());
+            }
+            else if(id == R.id.navigation_info) {
+                replaceFragment(new InfoFragment());
+            }
+            return true;
+        });
+
+        binding.bottomNavigationView.setBackground(null);
+    }
+    private void replaceFragment (Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
