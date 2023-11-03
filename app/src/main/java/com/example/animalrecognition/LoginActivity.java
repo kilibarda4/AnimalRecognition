adfa6401e6693bc8d640c2ffc0c7a4ad9cce6610
kilibarda4 <nxk4192@mavs.uta.edu>
2023-11-03 02:46:55 -0500

    added: Firebase auth

--- app/src/main/java/com/example/animalrecognition/LoginActivity.java
+++ app/src/main/java/com/example/animalrecognition/LoginActivity.java
@@ -1,21 +1,28 @@
 package com.example.animalrecognition;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.text.TextUtils;
-import android.text.method.HideReturnsTransformationMethod;
-import android.view.View;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class LoginActivity extends AppCompatActivity {
 
-    EditText username1, password1;
-    Button login1, signup1;
+    private static final String TAG = "LoginActivity";
+    private FirebaseAuth mAuth;
+    EditText emailLogin, passwordLogin;
+    Button buttonLogin, goToSignup, forgotPassword;
     DBHelper DB;
 
     @Override
@@ -23,41 +30,91 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        username1 = findViewById(R.id.username1);
-        password1 = findViewById(R.id.password1);
-        login1   = findViewById(R.id.login1);
-        signup1  = findViewById(R.id.signup1);
-        DB       = new DBHelper(this);
+        mAuth     = FirebaseAuth.getInstance();
 
-        login1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String user = username1.getText().toString();
-                String pass = password1.getText().toString();
+        emailLogin = findViewById(R.id.emailLogin);
+        passwordLogin = findViewById(R.id.passwordLogin);
+        buttonLogin    = findViewById(R.id.buttonLogin);
+        goToSignup   = findViewById(R.id.goToSignup);
+        forgotPassword = findViewById(R.id.forgotPassword);
 
-                if(TextUtils.isEmpty(user) || TextUtils.isEmpty(pass)) {
-                    Toast.makeText(LoginActivity.this, "All fields Required", Toast.LENGTH_SHORT).show();
-                }
-                else{
-                    Boolean checkUserPass = DB.checkUsernamePassword(user, pass);
-                    if(checkUserPass) {
-                        Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
-                        Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
-                        startActivity(intent);
-                    }else{
-                        Toast.makeText(LoginActivity.this, "Invalid Credentials", Toast.LENGTH_SHORT).show();
+//        login logic
+        buttonLogin.setOnClickListener(view -> {
+            String email = emailLogin.getText().toString().trim();
+            String password = passwordLogin.getText().toString().trim();
+
+            if(email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(LoginActivity.this, "All fields Required", Toast.LENGTH_SHORT).show();
+            } else {
+                signIn(email, password);
+            }
+        });
+
+        //go to registration page
+        goToSignup.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), RegistrationActivity.class);
+            startActivity(intent);
+        });
+
+        forgotPassword.setOnClickListener(view -> {
+            String email = emailLogin.getText().toString().trim();
+
+            if(email.isEmpty()) {
+                Toast.makeText(LoginActivity.this, "Please enter email address", Toast.LENGTH_SHORT).show();
+            } else {
+                sendPasswordReset(email);
+            }
+        });
+
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            updateUI(currentUser);
+        }
+    }
+    private void signIn(String email, String password) {
+        // [START sign_in_with_email]
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
                     }
-                }
-            }
-        });
+                });
+    }
+    private void updateUI(FirebaseUser user) {
+        if(user != null) {
+            Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+            startActivity(intent);
+        }
+    }
+    public void sendPasswordReset(String email) {
+        // [START send_password_reset]
+        FirebaseAuth auth = FirebaseAuth.getInstance();
 
-        signup1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(intent);
-            }
-        });
-
+        auth.sendPasswordResetEmail(email)
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        Log.d(TAG, "Email sent.");
+                        Toast.makeText(LoginActivity.this,
+                                "An email has been sent to reset your password",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
