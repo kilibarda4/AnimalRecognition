ec4dcafbceb6a8b30ebab341711e00b6b39fb666
kilibarda4 <nxk4192@mavs.uta.edu>
2023-11-04 22:47:35 -0500

    add: dialogAlert style

--- app/src/main/java/com/example/animalrecognition/ProfileFragment.java
+++ app/src/main/java/com/example/animalrecognition/ProfileFragment.java
@@ -13,6 +13,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class ProfileFragment extends Fragment {
@@ -27,7 +28,24 @@ public class ProfileFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
         logout = view.findViewById(R.id.logout);
         logout.setOnClickListener(v -> {
-            showLogoutConfirmationDialog();
+//            showLogoutConfirmationDialog();
+            androidx.appcompat.app.AlertDialog dialogBuilder =
+                    new MaterialAlertDialogBuilder(requireContext(),R.style.MyThemeOverlay_MaterialComponents_MaterialAlertDialog)
+                            .setMessage("Are you sure you want to sign out?")
+                            .setPositiveButton(android.R.string.yes, (dialog, which) -> {
+                                // Sign out the user from Firebase (if needed)
+                                FirebaseAuth.getInstance().signOut();
+
+                                // Navigate back to the LoginActivity
+                                Intent intent = new Intent(getActivity(), LoginActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity(intent);
+                            })
+                            .setNegativeButton(android.R.string.no, (dialog, which) -> {
+                                // User clicked No, dismiss the dialog
+                                dialog.dismiss();
+                            })
+                            .show();
         });
         return view;
     }
