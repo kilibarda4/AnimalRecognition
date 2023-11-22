18345976fd9dcb194e11ac1d6ba3802519392151
kilibarda4 <nxk4192@mavs.uta.edu>
2023-11-22 02:02:02 -0600

    add: record func

--- app/src/main/java/com/example/animalrecognition/HomeActivity.java
+++ app/src/main/java/com/example/animalrecognition/HomeActivity.java
@@ -1,28 +1,44 @@
 package com.example.animalrecognition;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.view.WindowCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
+import android.Manifest;
 import com.example.animalrecognition.databinding.ActivityHomeBinding;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.Firebase;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.analytics.FirebaseAnalytics;
 
+import java.io.IOException;
+
 public class HomeActivity extends AppCompatActivity {
 
+    private static final String LOG_TAG = "HomeActivity";
+    private static String fileName = null;
+    private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
+    private boolean permissionToRecordAccepted = false;
+    private String [] permissions = {Manifest.permission.RECORD_AUDIO};
     Button startRecordingBtn, stopRecordingBtn;
     TextView result;
     BottomNavigationView bottomNavigationView;
+
+    private MediaRecorder recorder = null;
+    private MediaPlayer player = null;
     ActivityHomeBinding binding;
     private FirebaseAnalytics mFirebaseAnalytics;
     @Override
@@ -31,6 +47,10 @@ public class HomeActivity extends AppCompatActivity {
         binding = ActivityHomeBinding.inflate(getLayoutInflater());
         WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
         setContentView(binding.getRoot());
+
+        fileName = getExternalCacheDir().getAbsolutePath() + "/audio.3gp";
+
+        ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION);
 
         //initialize firebase for the project
         FirebaseApp.initializeApp(this);
@@ -62,7 +82,14 @@ public class HomeActivity extends AppCompatActivity {
 
         Bundle params = new Bundle();
         params.putString("startRCD", "startRecordingButton");
-        startRecordingBtn.setOnClickListener(view -> mFirebaseAnalytics.logEvent("record_button_click", params));
+        startRecordingBtn.setOnClickListener(view -> {
+            mFirebaseAnalytics.logEvent("record_button_click", params);
+            startRecording();
+        });
+
+        stopRecordingBtn.setOnClickListener((v -> {
+            stopRecording();
+        }));
 
         binding.bottomNavigationView.setBackground(null);
     }
@@ -72,4 +99,55 @@ public class HomeActivity extends AppCompatActivity {
         fragmentTransaction.replace(R.id.frame_layout, fragment);
         fragmentTransaction.commit();
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_RECORD_AUDIO_PERMISSION) {
+            permissionToRecordAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+        }
+        if (!permissionToRecordAccepted ) finish();
+
+    }
+
+    private void startRecording() {
+        if (recorder == null) {
+            recorder = new MediaRecorder();
+            recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            recorder.setOutputFile(fileName);
+            recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+            try {
+                recorder.prepare();
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "prepare() failed");
+            }
+
+            recorder.start();
+        }
+    }
+
+    private void stopRecording() {
+        if (recorder != null) {
+            recorder.stop();
+            recorder.release();
+            recorder = null;
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (recorder != null) {
+            recorder.release();
+            recorder = null;
+        }
+
+        if (player != null) {
+            player.release();
+            player = null;
+        }
+    }
+
 }
