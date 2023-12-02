7141f7157c7a4b842ae6f706e89299601a091fb9
kilibarda4 <nxk4192@mavs.uta.edu>
2023-12-02 02:13:27 -0600

    add: filter out non animal sounds

--- app/src/main/java/com/example/animalrecognition/HomeActivity.java
+++ app/src/main/java/com/example/animalrecognition/HomeActivity.java
@@ -15,25 +15,18 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 
 import android.media.AudioRecord;
-import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 import android.Manifest;
 import android.widget.Toast;
 
 import com.example.animalrecognition.databinding.ActivityHomeBinding;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.analytics.FirebaseAnalytics;
-import com.google.firebase.ml.modeldownloader.CustomModel;
-import com.google.firebase.ml.modeldownloader.CustomModelDownloadConditions;
-import com.google.firebase.ml.modeldownloader.DownloadType;
-import com.google.firebase.ml.modeldownloader.FirebaseModelDownloader;
 
 import org.tensorflow.lite.Interpreter;
 import org.tensorflow.lite.support.audio.TensorAudio;
@@ -42,19 +35,17 @@ import org.tensorflow.lite.task.audio.classifier.AudioClassifier;
 import org.tensorflow.lite.task.audio.classifier.Classifications;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
-import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class HomeActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = "HomeActivity";
-//    private static String fileName = null;
     private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
     private boolean permissionToRecordAccepted = false;
     private String[] permissions = {Manifest.permission.RECORD_AUDIO};
@@ -84,53 +75,35 @@ public class HomeActivity extends AppCompatActivity {
         WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
         setContentView(binding.getRoot());
 
-//        fileName = getExternalCacheDir().getAbsolutePath() + "/audio.wav";
-
         ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION);
 
-        //initialize firebase for the project
+        //init firebase
         FirebaseApp.initializeApp(this);
         mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
 
-        //ML model download and update
-//        CustomModelDownloadConditions conditions = new CustomModelDownloadConditions.Builder()
-//                .requireWifi()
-//                .build();
-//        FirebaseModelDownloader.getInstance()
-//                .getModel("Animal-Classifier", DownloadType.LOCAL_MODEL, conditions)
-//                .addOnSuccessListener(model -> {
-//                    File modelFile = model.getFile();
-//                    if(modelFile != null) {
-//                        try {
-//                            interpreter = new Interpreter(model.getFile());
-//                            Toast.makeText(HomeActivity.this, "LOADED", Toast.LENGTH_LONG).show();
-//                        } catch (Exception e) {
-//                            Log.e("HomeActivity", "Interpreter Loading Failed", e);
-//                            Toast.makeText(HomeActivity.this, "FAILED", Toast.LENGTH_LONG).show();
-//                        }
-//                    }
-//                });
-
-        //fit predict
-
-
-        //initialize buttons
         btnStartRecording = findViewById(R.id.btnStartRecording);
         btnStopRecording = findViewById(R.id.btnStopRecording);
         result = findViewById(R.id.result);
         specs = findViewById(R.id.specs);
 
+        AtomicBoolean home = new AtomicBoolean(false);
         //navigate using the nav bar
         binding.bottomNavigationView.setOnItemSelectedListener(item -> {
             int id = item.getItemId();
             if (id == R.id.navigation_home) {
-                Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
-                startActivity(intent);
+                if(!home.get()) {
+                    Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                    startActivity(intent);
+                    home.set(true);
+                }
             } else if (id == R.id.navigation_profile) {
+                home.set(false);
                 replaceFragment(new ProfileFragment());
             } else if (id == R.id.navigation_stats) {
+                home.set(false);
                 replaceFragment(new StatsFragment());
             } else if (id == R.id.navigation_info) {
+                home.set(false);
                 replaceFragment(new InfoFragment());
             }
             return true;
@@ -141,7 +114,6 @@ public class HomeActivity extends AppCompatActivity {
 
         btnStartRecording.setOnClickListener(view -> {
             mFirebaseAnalytics.logEvent("record_button_click", params);
-//            startRecording();
             try {
                 classifier = AudioClassifier.createFromFile(this, modelPath);
             } catch (IOException e) {
@@ -169,11 +141,15 @@ public class HomeActivity extends AppCompatActivity {
                     List<Category> finalOutput = new ArrayList<>();
                     for(Classifications classifications : output) {
                         for(Category category : classifications.getCategories()) {
-                            if(category.getScore() > probabilityThreshold) {
+                            if(category.getScore() > probabilityThreshold
+                                    && category.getIndex() >= 69
+                                    && category.getIndex() <= 131) {
                                 finalOutput.add(category);
                             }
                         }
                     }
+
+
                     Collections.sort(finalOutput, (o1, o2) -> (int) (o1.getScore() - o2.getScore()));
 
                     StringBuilder outputStr = new StringBuilder();
@@ -181,14 +157,11 @@ public class HomeActivity extends AppCompatActivity {
                         outputStr.append(category.getLabel()).append(": ")
                                 .append(category.getScore()).append("\n");
                     }
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            if (finalOutput.isEmpty()) {
-                                result.setText("Could not classify");
-                            } else {
-                                result.setText(outputStr.toString());
-                            }
+                    runOnUiThread(() -> {
+                        if (!finalOutput.isEmpty()) {
+                            result.setText(outputStr.toString());
+                        } else {
+                            result.setText(R.string.result);
                         }
                     });
 
@@ -245,40 +218,3 @@ public class HomeActivity extends AppCompatActivity {
         builder.show();
     }
 }
-//    private void startRecording() {
-//        if (recorder == null) {
-//            recorder = new MediaRecorder();
-//            recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-//            recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-//            recorder.setOutputFile(fileName);
-//            recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-//
-//            try {
-//                recorder.prepare();
-//                recorder.start();
-//            } catch (IOException | IllegalStateException e) {
-//                Log.e(LOG_TAG, "prepare() failed");
-//                Toast.makeText(HomeActivity.this, "Failed to start recording", Toast.LENGTH_LONG).show();
-//            }
-//            Toast.makeText(HomeActivity.this, fileName, Toast.LENGTH_LONG).show();
-//            Log.e(LOG_TAG, fileName);
-//        }
-//    }
-//
-//    private void stopRecording() {
-//        if (recorder != null) {
-//            recorder.stop();
-//            recorder.release();
-//            recorder = null;
-//        }
-//    }
-//
-//    @Override
-//    public void onStop() {
-//        super.onStop();
-//        if (recorder != null) {
-//            recorder.release();
-//            recorder = null;
-//        }
-//    }
-//}
