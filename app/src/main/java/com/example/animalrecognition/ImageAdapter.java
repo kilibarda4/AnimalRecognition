59ccdaa715ea59912f776b74011bd0c79037aadd
Rog11 <36230617+Rog11@users.noreply.github.com>
2023-12-06 14:29:50 -0600

    added image sliders

---
+++ app/src/main/java/com/example/animalrecognition/ImageAdapter.java
@@ -0,0 +1,47 @@
+package com.example.animalrecognition;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ImageViewHolder> {
+
+    private List<Integer> images;
+
+    public ImageAdapter(List<Integer> images) {
+        this.images = images;
+    }
+
+    @NonNull
+    @Override
+    public ImageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_image, parent, false);
+        return new ImageViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ImageViewHolder holder, int position) {
+        holder.imageView.setImageResource(images.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return images.size();
+    }
+
+    static class ImageViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageView;
+
+        ImageViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.imageView);
+        }
+    }
+}
