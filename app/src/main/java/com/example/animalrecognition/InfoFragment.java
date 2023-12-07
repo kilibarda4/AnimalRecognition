7e3de7e52bf0906c19f47740d744f7203a793cc3
AliAlumary <axa3680@mavs.uta.edu>
2023-12-06 21:39:29 -0600

    added info

--- app/src/main/java/com/example/animalrecognition/InfoFragment.java
+++ app/src/main/java/com/example/animalrecognition/InfoFragment.java
@@ -1,31 +1,25 @@
 package com.example.animalrecognition;
+import android.widget.Button;
 
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-import androidx.viewpager2.widget.ViewPager2;
-
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
 import java.util.Arrays;
 import java.util.List;
 
 public class InfoFragment extends Fragment {
 
-    public InfoFragment() {
-        // Required empty public constructor
-    }
+    // ... (your existing code)
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-//        return inflater.inflate(R.layout.fragment_info, container, false);
-
         View view = inflater.inflate(R.layout.fragment_info, container, false);
 
         ViewPager2 viewPager = view.findViewById(R.id.viewPager);
@@ -39,10 +33,23 @@ public class InfoFragment extends Fragment {
         ImageAdapter imageAdapter = new ImageAdapter(images);
         viewPager.setAdapter(imageAdapter);
 
+        // Find the Button by its ID
+        Button learnMoreButton = view.findViewById(R.id.learnMoreButton);
+
+        // Set OnClickListener
+        learnMoreButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openWebPage();
+            }
+        });
+
         return view;
     }
 
-    public void openWebPage(View view) {
+    // ... (your existing code)
+
+    private void openWebPage() {
         // Handle the click event, e.g., open a web browser with the URL
         String url = getString(R.string.model_webpage); // Get the URL from resources
         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
